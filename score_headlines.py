7915f6faa8355162dabf35c2246f43c02bbeef44
apoorvagupta-12 <apoorvagupta.1208@gmail.com>
2025-07-06 17:52:30 -0500

    modular coding
    removed unneeded print statements, created helper functions to minimize work done in main.

--- score_headlines.py
+++ score_headlines.py
@@ -5,49 +5,70 @@ from datetime import date
 import joblib
 from sentence_transformers import SentenceTransformer
 
-# Check if we have the right number of arguments
-if len(sys.argv) != 3:
-    print("Usage: python score_headlines.py <file_with_headlines.txt> <source of headlines>")
-    sys.exit(1)
+MODEL_PATH = './assignment/svm.joblib'
 
-# Get the parameters
-headline_file_name = sys.argv[1]
-headline_source = sys.argv[2]
+def check_params():
+    """ Take input from the user and ensure it's in the correct format. """
 
-# print(headline_file_name)
-# print(headline_source)
+    if len(sys.argv) != 3:
+        print("Usage: python score_headlines.py <file_with_headlines.txt> <source of headlines>")
+        sys.exit(1)
 
-# open file as txt and convert headlines to vectors
+    return sys.argv[1], sys.argv[2]
 
-try:
-    with open(headline_file_name, 'r', encoding='utf-8') as f:
-        headlines = [line.strip() for line in f if line.strip()]
-except FileNotFoundError:
-    print(f"File {headline_file_name} not found.")
-    sys.exit(1)
+def load_headlines(filename):
+    """ Load headlines from the filename provided. """
 
-encoder = SentenceTransformer("all-MiniLM-L6-v2")
-headline_vectors = encoder.encode(headlines)
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            headlines = [line.strip() for line in f if line.strip()]
+            return headlines
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        sys.exit(1)
 
-# print(headlines)
+def create_vectors(headlines):
+    """ Create encoded vectors per guidance from loaded headlines. """
 
-# load model
+    encoder = SentenceTransformer("all-MiniLM-L6-v2")
+    return encoder.encode(headlines)
 
-model = joblib.load('./assignment/svm.joblib')
+def load_model(model_path):
+    """ Load the saved model provided. """
 
-# make predictions
+    return joblib.load(model_path)
 
-predictions = model.predict(headline_vectors)
+def get_output_filename(source):
+    """ Create output file name per standard provided. """
 
-# print(predictions)
+    today = date.today()
+    return f"headlines_scored_{source}_{today.year}_{today.month}_{today.day}.txt"
 
-# create variable that has output filename in format
+def make_predictions(model, vectors):
+    """ Make sentiment predictions from model. """
 
-today = date.today()
-OUTPUT_FILENAME = f"headlines_scored_{headline_source}_{today.year}_{today.month}_{today.day}.txt"
+    return model.predict(vectors)
 
-# print(output_filename)
+def save_file(filename, headlines, predictions):
+    """ Save output file in the format provided. """
 
-with open(OUTPUT_FILENAME, "w", encoding="utf-8") as f:
-    for headline, pred in zip(headlines, predictions):
-        f.write(f"{pred}, {headline}\n")
+    with open(filename, "w", encoding="utf-8") as f:
+        for headline, pred in zip(headlines, predictions):
+            f.write(f"{pred}, {headline}\n")
+
+    print("Output file is created and saved!")
+
+def main():
+    """ Main method, to call helper functions written. """
+
+    headline_file_name, headline_source = check_params()
+    headlines = load_headlines(headline_file_name)
+    headline_vectors = create_vectors(headlines)
+    model = load_model(MODEL_PATH)
+    predictions = make_predictions(model, headline_vectors)
+    output_filename = get_output_filename(headline_source)
+    save_file(output_filename, headlines, predictions)
+
+
+if __name__ == "__main__":
+    main()
