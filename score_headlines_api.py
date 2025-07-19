22ef6b96619675be5e767b60ce83fe36a4a4727b
apoorvagupta-12 <apoorvagupta.1208@gmail.com>
2025-07-19 16:29:38 -0500

    added logs to server

--- score_headlines_api.py
+++ score_headlines_api.py
@@ -9,6 +9,10 @@ import joblib
 from datetime import date
 import os
 import uvicorn
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -22,7 +26,7 @@ encoder = SentenceTransformer(ENCODER_MODEL)
 @app.get("/status")
 def status():
     """GET Status API call"""
-
+    logger.info("Received GET request for /status")
     return JSONResponse({"status" : "ok"})
 
 class HeadlineRequest(BaseModel):
@@ -31,15 +35,20 @@ class HeadlineRequest(BaseModel):
 @app.post("/score_headlines")
 def score_headlines(request: HeadlineRequest):
     """POST API Call to score headlines"""
+    logger.info("Received POST request for /score_headlines")
+
 
     if not request.headlines:
+        logger.info("No headlines provided")
         raise HTTPException(status_code=400, detail="Please enter a headline")
 
     try:
         vectors = encoder.encode(request.headlines)
         predictions = model.predict(vectors)
+        logger.info("Predictions made and returned")
         return JSONResponse(content={"predictions": predictions.tolist()})
     except Exception as e:
+        logger.warning("Error making predictions")
         raise HTTPException(status_code=500, detail=f"Unable to make predictions. Please try again. {str(e)}")
 
 
